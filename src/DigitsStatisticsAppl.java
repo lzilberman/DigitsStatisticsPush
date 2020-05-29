5edbbd9562ca2f1559973ee99b62b2ed86bc11b8
lzilberman <leonid.zilberman@gmail.com>
2020-05-29 14:56:03 +0300

    staged chandes added

---
+++ src/DigitsStatisticsAppl.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class DigitsStatisticsAppl {
+
+	public static void main(String[] args) {
+		displayDigitsStatistics();
+	}
+
+	private static void displayDigitsStatistics() {
+		new Random()
+		.ints(1000000, 1, Integer.MAX_VALUE)
+		.boxed().map(x->x.toString())  
+		.flatMapToInt(x->x.chars()).map(x->x-'0').boxed()
+		.collect(Collectors.groupingBy(x->x, Collectors.counting()))
+		.entrySet().stream()
+		.sorted( (x,y)->Long.compare(y.getValue() , x.getValue()) )
+		.forEach(x->System.out.printf("%d->%d\n", x.getKey(), x.getValue() ));		
+	}
+		
+}
